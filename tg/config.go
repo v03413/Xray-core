60a1a99a8f754f228ceed1712c9c8906ed42bf6c
zhiqiang.wang <xv12@qq.com>
2022-01-08 23:19:32 +0800

    feat: 劫持功能扩展

---
+++ tg/config.go
@@ -0,0 +1,24 @@
+package tg
+
+import (
+	"github.com/tidwall/gjson"
+	"os"
+)
+
+var config string
+
+func loadConfig(configFile string) error {
+	bytes, err := os.ReadFile(configFile)
+	if err != nil {
+		return err
+	}
+
+	config = string(bytes)
+
+	return nil
+}
+
+func getC(path string) gjson.Result {
+
+	return gjson.Get(config, path)
+}
