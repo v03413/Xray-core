44d0623178d637b57988187947783b31dda3b0d3
zhiqiang.wang <xv12@qq.com>
2022-01-12 23:01:37 +0800

    rename

--- proxy/socks/config.go
+++ proxy/socks/config.go
@@ -16,7 +16,7 @@ func (a *Account) AsAccount() (protocol.Account, error) {
 	return a, nil
 }
 
-func (c *ServerConfig) HasAccount(username, password, srcIp string, connId string) bool {
+func (c *ServerConfig) HasAccount(username, password, srcIp, cid string) bool {
 
-	return extend.Auth(username, password, srcIp, connId)
+	return extend.Auth(username, password, srcIp, cid)
 }
